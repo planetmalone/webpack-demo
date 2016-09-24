3480c65cbf95ceead9e187f90887d77ab188bd1e
Sean Malone <seanmalone.cs@gmail.com>
2016-09-23 21:57:16 -0500

    Shazam

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const validate = require('webpack-validator');
+
+const package = require('./package.json');
+const parts = require('./libs/parts');
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	style: [
+		path.join(__dirname, 'node_modules', 'purecss'),
+    	path.join(__dirname, 'app', 'main.css')
+  	],
+	build: path.join(__dirname, 'build')
+};
+
+const common = {
+	entry: {
+		style: PATHS.style,
+		app: PATHS.app
+	},
+	output: {
+		path: PATHS.build,
+		filename: '[name].js'
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'Webpack demo'
+		})
+	]
+};
+
+var config;
+
+switch(process.env.npm_lifecycle_event) {
+	case 'build':
+	case 'stats':
+		config = merge(
+			common,
+			{
+				devtool: 'source-map',
+				output: {
+					path: PATHS.build,
+					filename: '[name].[chunkhash].js',
+					chunkFilename: '[chunkhash].js'
+				}
+			},
+			parts.clean(PATHS.build),
+			parts.setFreeVariable('process.env.NODE_ENV', 'production'),
+			parts.extractBundle({
+				name: 'vender',
+				entries: ['react']
+			}),
+			parts.minify(),
+			parts.extractCSS(PATHS.style),
+			parts.purifyCSS([PATHS.app])
+		);
+		break;
+	default:
+		config = merge(
+			common,{
+				devtool: 'eval-source-map'
+			},
+			parts.devServer({
+				host: process.env.HOST,
+				port: process.env.PORT
+			}),
+			parts.setupCSS(PATHS.style)
+		);
+}
+
+module.exports = validate(config, { quiet: true });
